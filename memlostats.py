d272b4655f8878998068dc7c28cecfcb8b3c7090
Matt Miller <mattmiller87@gmail.com>
2018-02-15 14:12:26 -0800

    fuck pprint. this works and sends json data to discord

--- memlostats.py
+++ memlostats.py
@@ -44,9 +44,8 @@ class memlostats:
                 else:
                         playerdata = rocket.players.player(id=gamertag, platform=platformid)
                         playerjson = str(playerdata.json())
-#                        with open(self.json, "w") as f:
-#                                f.write(str(playerdata.json()))
-                        pprint.pprint(playerjson, self.json)
+                        with open(self.json, "w") as f:
+                                f.write(str(playerdata.json()))
                         opener=urllib.request.build_opener()
                         opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
                         urllib.request.install_opener(opener)
