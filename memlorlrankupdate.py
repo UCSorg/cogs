f029a2a6b7117be2ce70245b56d53d019c54b941
Matt Miller <mattmiller87@gmail.com>
2018-02-15 11:29:17 -0800

    defining more functions
    making this a bit more modular

--- memlorlrankupdate.py
+++ memlorlrankupdate.py
@@ -11,6 +11,8 @@ class memlorlrankupdate:
         def __init__(self, bot):
                 self.bot = bot
                 self.image = "data/rlstats/signature.png"
+                self.json = "data/rlstats/rlstats.json"
+                self.legend = "data/rlstas/tierlegend.json"
 
         @commands.command(pass_context=True)
         async def gadget(self, ctx):
@@ -20,50 +22,79 @@ class memlorlrankupdate:
                 author = str(ctx.message.author)
                 data = ctx.message.content.strip()
                 if "gadget" in data:
-                        await self.bot.say("Hey `" + author + "`!  Can I get your steamID?")
-                        responsedirty = await self.bot.wait_for_message(author=ctx.message.author)
-                        response = responsedirty.content.lower().strip()
-                        if responsedirty == "none":
-                                pass
+                        steamiddict = await self.steamid(ctx)
+                        if "success" in steamiddict['1']:
+                                rankdict = await self.getrank(steamiddict['2'], steamiddict['3'])
+                                if "success" in rankdict['result']:
+                                        await self.bot.send_file(channel, self.image)
+                                        pass
+                                else:
+                                        await self.bot.say(returndata)
                         else:
+                                pass
 
-                                try:
-                                        await self.bot.say("I will try `" + response + "`")
-#                                       await self.bot.say("Hang on,I'm going to try something.  Hold my hand, I'm scared")
-                                        data = self.getrank(channel, author, response)
-                                        if "success" in data:
-                                                await self.bot.send_file(channel, self.image)
-                                                await self.bot.say("Hey `" + author + "` is this you?")
-                                        else: await self.bot.say(data)
-                                except AttributeError:
-                                        await self.bot.say("Looks like I ran into an exception")
+#               try:
+#                       returndata = self.getrank(channel, author, response, response2)
+#                                               if "success" in returndata['result']:
+#                                                       await self.bot.send_file(channel, self.image)
+#                                                       await self.bot.say("Hey `" + author + "` is this you?")
+#                                               else:
+#                                                       await self.bot.say(returndata)
+#                                       except AttributeError:
+#                                               await self.bot.say("Looks like I ran into an exception")
 #                               except TypeError:
 #                                       await self.bot.say("I hit an exception wall, it's probably me, not you.  You're perfect.  Definitely not you.")
-                        responsedirty = await self.bot.wait_for_message(author=ctx.message.author)
-                        response = responsedirty.content.lower().strip()
-                        if responsedirty == "none":
-                                pass
-                        elif "yes" in response:
-                                await self.bot.say("Here's where I would set your rank.")
-                        elif "no" in response:
-                                await self.bot.say("We might need to start over.")
-                        else:
-                                await self.bot.say("Your answer wasn't  worth my time.")
+#                       response3dirty = await self.bot.wait_for_message(author=ctx.message.author)
+#                       response3 = response3dirty.content.lower().strip()
+#                       if response3dirty == "none":
+#                               pass
+#                       elif "yes" in response:
+#                               rank = parseforrank()
+#                               await self.bot.say("Here's where I would set your rank to " + rank)
+#                       elif "no" in response:
+#                               await self.bot.say("We might need to start over.")
+#                       else:
+#                               await self.bot.say("Your answer wasn't  worth my time.")
 
-        def getrank(self, channel, author, steamidinput):
+        async def steamid(self, ctx):
+                author = str(ctx.message.author)
+                await self.bot.say("Hey `" + author + "`!  Can I get your steamID?")
+                steamidresponse = await self.bot.wait_for_message(author=ctx.message.author)
+                if steamidresponse == "none":
+                        pass
+                else:
+                        await self.bot.say("Are you on **PC**, **PS4**, or **XBOX**?  note: Switch not supported currently")
+                        platformresponse = await self.bot.wait_for_message(author=ctx.message.author)
+                        if platformresponse == "none":
+                                pass
+                        else:
+                                steamid = steamidresponse.content.lower().strip()
+                                platform = platformresponse.content.lower().strip()
+                                dict = { '1': 'success', '2' : steamid, '3' : platform}
+                                await self.bot.say("I will try `" + steamid + "` on `" + platform + "`." )
+                                return dict
+
+        def getrank(self, channel, author, steamidinput, platforminput):
                 """Retrieves Rocket League Stats image from rocketleaguestats.com using their API sends image back"""
                 rocket = RocketLeague(api_key='ZEP7NZ0WLD9AFJ8WU15JZU5XD1XKM3TO')
                 steamid = str(steamidinput)
-                response = rocket.players.player(id=steamidinput, platform=1)
-                signatureUrl = response.json()['signatureUrl']
-
+                platform = str(platforminput)
+                if "pc" in platform.lower():
+                        response = rocket.players.player(id=steamidinput, platform=1)
+                        self.json = response.jason()
+                elif "ps4" in platform.lower():
+                        response = rocket.players.player(id=steamidinput, platform=2)
+                        signatureUrl = response.json()['signatureUrl']
+                elif "xbox" in platform.lower():
+                        response = rocket.players.player(id=steamidinput, platform=3)
+                        signatureUrl = response.json()['signatureUrl']
                 try:
                         opener=urllib.request.build_opener()
                         opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
                         urllib.request.install_opener(opener)
                         urllib.request.urlretrieve(signatureUrl, self.image)
-                        success = "success"
-                        return success
+                        result = "success"
+                        return result
                 except urllib.error.HTTPError:
                         error = "Welp, looks like I ran into an HTTP Error"
                         return error
@@ -78,6 +109,28 @@ class memlorlrankupdate:
                 await self.bot.send_file(channel, self.image)
                 await self.bot.say("Hey `" + author + "` is this you?")
 
+        def parseforrank():
+                """sort through self.json and return highest rank"""
+                latestseason = "7"
+                rank = self.json['rankedSeasons'][latestseason]
+                rank1v1 = rank['10']
+                rank2v2 = rank['11']
+                rank3ss = rank['12']
+                rank3v3 = rank['13']
+                ranks = [rank1v1,rank2v2,rank3ss, rank3v3]
+                maxrank = str(max(ranks))
+                return maxrank
+                for k,v in self.legend.items():
+                        if maxrank == k:
+                                namedrank = v
+                                break
+                try:
+                        namedrank
+                except NameError:
+                        error = "Welp, a NameError occurred when looking at ranks"
+                        return error
+                else:
+                        return namedrank
 
 def setup(bot):
         action = memlorlrankupdate(bot)
