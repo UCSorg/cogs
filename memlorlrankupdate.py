6e03866fd35ead94878c57ca04e5a29bced40ba2
Matt Miller <mattmiller87@gmail.com>
2018-02-15 12:03:11 -0800

    steamid

--- memlorlrankupdate.py
+++ memlorlrankupdate.py
@@ -23,11 +23,10 @@ class memlorlrankupdate:
                 data = ctx.message.content.strip()
                 if "gadget" in data:
                         steamiddict = await self.steamid(ctx)
-                        steamid = str(steamiddict['2'].strip())
-                        platform = str(steamiddict['3'].strip())
-                        await self.bot.say(steamid + ", " + platform)
+                        steamid = steamiddict['2']
+                        platform = steamiddict['3']
                         if "success" in steamiddict['1']:
-                                rankdict = await self.getrank(steamid, platform)
+                                rankdict = self.getrank(steamiddict['2'], platform)
                                 if "success" in rankdict['result']:
                                         await self.bot.send_file(channel, self.image)
                                         pass
