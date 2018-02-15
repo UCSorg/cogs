b36ee4d58affb8d5fce97f025fab79d7e857cf2e
Matt Miller <mattmiller87@gmail.com>
2018-02-15 14:08:04 -0800

    pprint the json please

--- memlostats.py
+++ memlostats.py
@@ -43,9 +43,10 @@ class memlostats:
                         return error
                 else:
                         playerdata = rocket.players.player(id=gamertag, platform=platformid)
+                        playerjson = playerdata.json()
 #                        with open(self.json, "w") as f:
 #                                f.write(str(playerdata.json()))
-                        pprint.pprint(str(playerdata.json()), self.json)
+                        pprint.pprint(playerjson, self.json)
                         opener=urllib.request.build_opener()
                         opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
                         urllib.request.install_opener(opener)
