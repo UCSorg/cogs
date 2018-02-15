cc252f4d97a8836c641e8e961ecf0ba035b922a6
Matt Miller <mattmiller87@gmail.com>
2018-02-15 13:41:06 -0800

    .json() added

--- memlostats.py
+++ memlostats.py
@@ -45,7 +45,7 @@ class memlostats:
                         opener=urllib.request.build_opener()
                         opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
                         urllib.request.install_opener(opener)
-                        urllib.request.urlretrieve(playerdata['signatureUrl'], self.image)
+                        urllib.request.urlretrieve(playerdata.json()['signatureUrl'], self.image)
                         result = "success"
                         return result
 
