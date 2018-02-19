5244189f7abae6f9a935bc04c25cd85715ca0a4f
Matt Miller <mattmiller87@gmail.com>
2018-02-18 21:54:03 -0800

    +1

--- memlostats.py
+++ memlostats.py
@@ -31,6 +31,7 @@ class memlostats:
                                 if "Fail" in returnrank:
                                         pass
                                 else:
+                                        self.discordsendfile(channel, self.json)
                                         ranks = returnrank['rankedSeasons']
                                         await self.discordsay("Your highest rank is `" + ranks + "`.")
 
