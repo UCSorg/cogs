ce568f3ffbe4383ef82f77cf7bef06f5d2e0d570
Matt Miller <mattmiller87@gmail.com>
2018-03-01 11:39:34 -0800

    Image is working, embed is working

--- rlrank.py
+++ rlrank.py
@@ -45,7 +45,7 @@ class rlrank:
                                         await self.discordembed(channel, content)
                                 else:
 #                                        await self.discordsendfile(channel, self.image)
-                                        content = Embed(title=gamertag, description="Here are your Rocket League ranks: [" + gamertag + "](" + playerurl + ")", url=playerurl, color=10604116)
+                                        content = Embed(title="Here are the Rocket League ranks: [" + gamertag + "](" + playerurl + ")", url=playerurl, color=10604116)
                                         content.set_image(url=playersignature)
                                         await self.discordembed(channel, content)
 
