8096ec0cf98321b54ef87a5385a2e2947ca9d249
Matt Miller <mattmiller87@gmail.com>
2018-03-01 09:42:18 -0800

    testing playerurl

--- rlrank.py
+++ rlrank.py
@@ -60,8 +60,10 @@ class rlrank:
                                         await self.discordembed(channel, content)
                                 else:
 #                                        await self.discordsendfile(channel, self.image)
-                                        content = discord.Embed(title=gamertag, description="Here are your Rocket League ranks: [" + gamertag + "](" + playerurl + ")", url=playerurl, color=10604116, image=playersignature)
-                                        await self.discordembed(channel, content)
+                                        await self.discordsay(playerurl)
+                                        await self.discordsay(playersignature)
+#                                        content = discord.Embed(title=gamertag, description="Here are your Rocket League ranks: [" + gamertag + "](" + playerurl + ")", url=playerurl, color=10604116, image=playersignature)
+#                                        await self.discordembed(channel, content)
 
         def getrank(self, platform, gamertag):
                 """Retrieves Rocket League Stats image from rocketleaguestats.com using their API sends image back"""
