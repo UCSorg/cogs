11c369c54ffd8910172858a63a42c4258a1f6ad4
Matt Miller <mattmiller87@gmail.com>
2018-03-07 09:40:06 -0800

    create file if not present

--- rlrank/rlrank.py
+++ rlrank/rlrank.py
@@ -15,19 +15,11 @@ tierlegend =    {1:"Bronze I", 2:"Bronze II",3:"Bronze III",4:"Silver I",5:"Silv
                 13:"Diamond I",14:"Diamond II",15:"Diamond III",16:"Champion I",17:"Champion II",18:"Champion III",19:"Grand Champion"}
 apidefault = {"key" : "Error"}
 
-def check_folders():
-    if not os.path.exists("data/rlrank"):
-        print("Creating data/rlrank folder...")
-        os.makedirs("data/rlrank")
-
 class rlrank:
         """Custom cog by Memlo and Eny, Matt Miller and Patrik Srna, that retrieves a user's Rocket League stats based on gamertag and platform input"""
 
         def __init__(self, bot):
                 self.bot = bot
-                if not os.path.isfile(apipath):
-
-                        dataIO.save_json(apipath, apidefault)
                 self.apikey = dataIO.load_json(apipath)
 
         @commands.group(pass_context=True, no_pm=True, invoke_without_command=True)
@@ -136,6 +128,19 @@ class rlrank:
                 """Simple embed in discord"""
                 await self.bot.send_message(channel, embed=content)
 
+def check_folders():
+    if not os.path.exists("data/rlrank"):
+        print("Creating data/rlrank folder...")
+        os.makedirs("data/rlrank")
+
+def check_files():
+    f = apipath
+    if not dataIO.is_valid_json(f):
+        print("Creating rls-apikey.json...")
+        dataIO.save_json(f, apidefault)
+
 def setup(bot):
+        check_folders()
+        check_files()
         action = rlrank(bot)
         bot.add_cog(action)
