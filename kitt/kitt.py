00bfc2c6707d7597e6a95f3f590c01f5ea2dd3cb
Matt Miller <mattmiller87@gmail.com>
2018-03-21 15:14:15 -0700

    +1

--- kitt/kitt.py
+++ kitt/kitt.py
@@ -16,7 +16,7 @@ import urllib
 #"I hit an exception wall, it's probably me, not you.  You're perfect.  Definitely not you."
 
 hubdatapath = "data/rlrank/hubdata.json"
-hubtdatadefault = {"discordid" : "player1Data"}
+hubtdatadefault = {}
 tierlegend =    {1:"Bronze I", 2:"Bronze II",3:"Bronze III",4:"Silver I",5:"Silver II",6:"Silver III",
                 7:"Gold I",8:"Gold II",9:"Gold III",10:"Platinum I",11:"Platinum II",12:"Platinum III",
                 13:"Diamond I",14:"Diamond II",15:"Diamond III",16:"Champion I",17:"Champion II",18:"Champion III",19:"Grand Champion"}
