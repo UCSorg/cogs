281ca3038681fd8542a6c332a2d8bca81bf7826e
Matt Miller <mattmiller87@gmail.com>
2018-03-21 10:08:11 -0700

    +1

--- rlrank/rlrank.py
+++ rlrank/rlrank.py
@@ -88,7 +88,7 @@ class rlrank:
                                                 await self.discordembed(channel, content)
                                                 await self.discordsendfile(channel, image)
                                                 tmp = dataIO.load_json(hubdatapath) #store the data about the player for use later
-                                                print(tmp)
+                                                dataIO.save_json(hubdatapath, tmp)
 
         async def discordsay(self, data):
                 """Simple text in discord"""
