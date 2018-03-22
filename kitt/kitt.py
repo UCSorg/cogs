d2c53d7d7473cd78c2711b591ccedcd90704c6b6
Matt Miller <mattmiller87@gmail.com>
2018-03-22 08:57:08 -0700

    +1

--- kitt/kitt.py
+++ kitt/kitt.py
@@ -117,14 +117,13 @@ class kitt:
                             except NameError:
                                 await self.bot.say("Fail. rlsapi NameError for API CURL Request - ask an admin")
                             else:
-                                if "code" in playerdata():
+                                if "code" in playerdata:
                                     error = "Fail. Error: %s. %s  gamertag=%s, platform=%s" % (str(playerdata.json()['code']),playerdata.json()['message'],gamertag,platformid)
                                     await self.bot.say(error)
-                                elif:
-                                    if "Fail" in playerdata: #if error code, respond with error code message
+                                elif "Fail" in playerdata: #if error code, respond with error code message
                                         content = Embed(title="Error", description=data, color=16713736)
                                         await self.discordembed(channel, content)
-                                    else:
+                                else:
                                         playerurl = playerdata.get("profileUrl")
                                         playersignature = playerdata.get("signatureUrl")
                                         try:
