df5bdd813c24a99552a848c98c93019a604fca5d
Matt Miller <mattmiller87@gmail.com>
2018-03-22 21:27:24 -0700

    +1

--- kitt/kitt.py
+++ kitt/kitt.py
@@ -56,32 +56,33 @@ class kitt:
                     await self.discordsay(todosplit)
                     while todosplit:
                         item = todosplit.pop(0)
-                        if item.lower() in nlpHelp:
-                                await self.discordsay(bothelp)
-                        elif item.lower() in nlpAboutMe:
-                                await self.kittaboutme(ctx)
-                        elif item.lower() in nlpSet:
-                                for item in todosplit:
-                                    if item.lower() in nlpBasic:
-                                        await self.kittbasicinfo(ctx)
-                                    elif item.lower() in nlpRegion:
-                                        await self.kittregion(ctx)
-                                    elif item.lower() in nlpRLRank:
-                                        await self.kittrlrank(ctx)
-                                    else:
-                                        todo = await self.question(ctx,"What can I help you set? Some keywords are: %s, %s, %s" % (nlpBasic[0], nlpRegion[0], nlpRLRank[0]))
-                        elif item.lower() in nlpRemove:
-                                for item in todosplt:
-                                    if item.lower() in nlpRemoveRole:
-                                        await self.kittremoverole(ctx)
-                                    elif item.lower() in nlpRemoveAboutMe:
-                                        await self.kittremovehubdata(ctx)
-                                    else:
-                                        todo = await self.question(ctx,"What can I help you remove? Some keywords are: %s, %s" % (nlpRemoveRole[0], nlpRemoveAboutMe[0]))
+                        while item:
+                            if item.lower() in nlpHelp:
+                                    await self.discordsay(bothelp)
+                            elif item.lower() in nlpAboutMe:
+                                    await self.kittaboutme(ctx)
+                            elif item.lower() in nlpSet:
+                                    for item in todosplit:
+                                        if item.lower() in nlpBasic:
+                                            await self.kittbasicinfo(ctx)
+                                        elif item.lower() in nlpRegion:
+                                            await self.kittregion(ctx)
+                                        elif item.lower() in nlpRLRank:
+                                            await self.kittrlrank(ctx)
+                                        else:
+                                            todo = await self.question(ctx,"What can I help you set? Some keywords are: %s, %s, %s" % (nlpBasic[0], nlpRegion[0], nlpRLRank[0]))
+                            elif item.lower() in nlpRemove:
+                                    for item in todosplt:
+                                        if item.lower() in nlpRemoveRole:
+                                            await self.kittremoverole(ctx)
+                                        elif item.lower() in nlpRemoveAboutMe:
+                                            await self.kittremovehubdata(ctx)
+                                        else:
+                                            todo = await self.question(ctx,"What can I help you remove? Some keywords are: %s, %s" % (nlpRemoveRole[0], nlpRemoveAboutMe[0]))
+                            else:
+                                continue
                         else:
-                            continue
-                    else:
-                                await self.discordsay("I'm not set up to do really anything else at this time.")   
+                                    await self.discordsay("I'm not set up to do really anything else at this time.")   
 
 
         async def kittbasicinfo(self, ctx):
