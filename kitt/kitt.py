eb8c0fb4aff5aa36763cc7d1a8ab26a9d09ad6e4
Matt Miller <mattmiller87@gmail.com>
2018-03-22 20:28:27 -0700

    +1

--- kitt/kitt.py
+++ kitt/kitt.py
@@ -52,7 +52,7 @@ class kitt:
                 if todo == None:
                         pass
                 else:
-                    todosplit = re.split('; |, | \*|\n',todo)
+                    todosplit = re.split('; |, | \* | \s|\n',todo)
                     await self.discordsay(todosplit)
                     while todosplit:
                         item = todosplit.pop(0)
